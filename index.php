90081298f199abac5b47070d684eb76cfb657669
Anastaisia <sam130117@gmail.com>
2017-11-28 23:10:25 +0200

    5

--- index.php
+++ index.php
@@ -48,6 +48,6 @@
 <div class="modal"></div>
 
 <script src="assets/js/jquery-3.2.1.min.js"></script>
-<script src="assets/js/script.js"></script>
+<!--<script src="assets/js/script.js"></script>-->
 </body>
 </html>
--- models/StringManager.php
+++ models/StringManager.php
@@ -37,21 +37,24 @@ class StringManager
     public static function process($string)
     {
         $array = preg_split("/[,|.|;|:|!|?|!| |-|.|:|;|?|(|)|{|}|’|‘|\n|\r|]/", $string, -1, PREG_SPLIT_NO_EMPTY);
+        $isFound = false;
+        $db = new PDO('mysql:host=localhost;dbname=wordnet;', 'root', '');
+
+
         if ($array) {
             for ($i = 0; $i < count($array) - 1; $i++) {
-
+                echo '-----------------------------<br/>';
                 if (in_array($array[$i], self::$partsOfSpeech)) {
-                    //if word is in array -> remove it because it is correct
+                    //if word is in array -> it is correct
                     $array[$i] = '-' . $array[$i] . '-';
+                    echo $array[$i] . ' word is in array -> correct<br/>';
                 } else {
                     foreach (self::$partsOfSpeech as $partsOfSpeech) {
                         //find levenshtein distance among $partsOfSpeech
                         //than find levenshtein distance among wordnet
                         //choose
                         $distance = levenshtein($array[$i], $partsOfSpeech, 1, 1, 1);
-                        if (strlen($array[$i]) && $distance < 2) {
-
-                            $db = new PDO('mysql:host=localhost;dbname=wordnet;', 'root', '');
+                        if (strlen($array[$i]) && $distance == 1) {
 
                             try {
                                 $stmt = $db->query("SELECT lemma, morph, pos, morphmaps.morphid FROM words " .
@@ -62,11 +65,15 @@ class StringManager
                                 if($result && ($result[0]['lemma'] || $result[0]['morph']))
                                 {
                                     $array[$i] = '-' . $array[$i] . '-';
+                                    $isFound = true;
+                                    echo $array[$i] . ' word with distance 1 in array -> found db -> replaced<br/>';
                                 }
                                 else {
                                     //get closest word from $partsOfSpeech
-                                    echo 'Distance for ' . $array[$i] . ' and ' . $partsOfSpeech . ' = ' . $distance . "<br/>";
+//                                    echo 'Distance for ' . $array[$i] . ' and ' . $partsOfSpeech . ' = ' . $distance . "<br/>";
                                     $array[$i] = '/' . $partsOfSpeech . '/';
+                                    $isFound = true;
+                                    echo $array[$i] . ' word with distance 1 in array -> not found in db, found closest in array -> replaced<br/>';
                                 }
                             } catch (PDOException $e) {
                                 return $e;
@@ -74,10 +81,67 @@ class StringManager
                         }
                     }
 
-                    //check other words in db
-//                    $distance = levenshtein($array[$i], $partsOfSpeech, 1, 1, 1);
-//                    echo 'Distance for ' . $array[$i] . ' and ' . $partsOfSpeech . ' = ' . $distance . "<br/>";
+                    echo $array[$i] . '<br/>';
+
+                    if(!$isFound)   // if the word was not found in $partsOfSpeech -> check other words in db
+                    {
+                        echo '+';
+                        echo $array[$i] . ' word was not found in array -> search in db<br/>';
+                        $similarWords = [];
+
+                        $length = strlen($array[$i]);
+
+                        $globalStmt = $db->query("SELECT lemma, morph FROM words " .
+                            "LEFT JOIN morphmaps ON morphmaps.wordid = words.wordid LEFT JOIN morphs ON " .
+                            "morphs.morphid = morphmaps.morphid WHERE LENGTH(lemma) = " . (int)($length) . " OR LENGTH(lemma) = " . (int)($length + 1) .
+                        " OR LENGTH(lemma) = " . (int)($length - 1) . " OR LENGTH(morph) = " . (int)($length) . " OR LENGTH(morph) = " . (int)($length + 1) .
+                        " OR LENGTH(morph) = " . (int)($length - 1));
+                        $globalResult = $globalStmt->fetchAll(PDO::FETCH_ASSOC);
+
+                        foreach ($globalResult as $item)
+                        {
+                            if($item['lemma'] != null)
+                            {
+                                if($array[$i] == $item['lemma'])
+                                {
+                                    $array[$i] = '-' . $array[$i] . '-';
+                                    echo $array[$i] . ' word from lemma found in db -> correct<br/>';
+                                }
+                                else
+                                {
+                                    $distance = levenshtein($array[$i], $item['lemma'], 1, 1, 1);
+                                    if($distance == 1)
+                                    {
+                                        echo 'Distance for ' . $array[$i] . ' and ' . $item['lemma'] . ' = ' . $distance . "<br/>";
+                                        $similarWords[] = $item['lemma'];
+                                        $array[$i] = '/' . $item['lemma'] . '/';
+                                    }
+                                }
+                            }
+                            else if($item['morph'] != null)
+                            {
+                                $distance = levenshtein($array[$i], $item['morph'], 1, 1, 1);
+                                if($array[$i] == $item['morph'])
+                                {
+                                    $array[$i] = '-' . $array[$i] . '-';
+                                    echo $array[$i] . ' word from morph found in db -> correct<br/>';
+                                }
+                                else
+                                {
+                                    if($distance == 1)
+                                    {
+                                        echo 'Distance for ' . $array[$i] . ' and ' . $item['morph'] . ' = ' . $distance . "<br/>";
+                                        $similarWords[] = $item['morph'];
+                                        $array[$i] = '/' . $item['morph'] . '/';
+                                    }
+                                }
+                            }
+                        }
+                        var_dump($similarWords);
+                    }
+                    $isFound = false;
                 }
+                echo '-----------------------------<br/>';
             }
         } else {
             $array = $string;
